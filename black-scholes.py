424e2ca2d1552944051eb53f9b0bc0673c1263b7
Kevin <kevinyin2013@gmail.com>
2023-05-12 13:40:37 -0700

    just for now

--- black-scholes.py
+++ black-scholes.py
@@ -2,10 +2,11 @@ import yfinance as yf
 import math
 import numpy as np
 import streamlit as st
+import yahooquery as yq
 
 def cdf(x):
     return (1 + math.erf(x))/2
-def blackscholes(spot, strike, risk_free, time, volatility):
+def blackscholes(spot, strike, risk_free, time, volatility, iscall=True):
     def d1():
         if ((volatility * math.sqrt(time)) > 0 and strike > 0):
             return (math.log(spot/strike) + (risk_free + (volatility**2)/2)*time)/(volatility * math.sqrt(time))
@@ -16,19 +17,22 @@ def blackscholes(spot, strike, risk_free, time, volatility):
         return first - volatility * math.sqrt(time)
     first = d1()
     second = d2()
-    return cdf(first) * spot - cdf(second)* strike * math.exp(-1 * risk_free * time)
-
+    if iscall:
+        return cdf(first) * spot - cdf(second)* strike * math.exp(-1 * risk_free * time)
+    else:
+        return cdf(-second)* strike * math.exp(-1 * risk_free * time) - cdf(-first) * spot
 
 st.title("Black Scholes")
+with st.sidebar:
+    spot = st.number_input('Spot price')
+    strike = st.number_input('Strike price')
+    risk_free = st.number_input('Risk Free')
+    time = st.number_input('Time')
+    volatility = st.number_input('Volatility')
 
-spot = st.number_input('Spot price')
-strike = st.number_input('Strike price')
-risk_free = st.number_input('Risk Free')
-time = st.number_input('Time')
-volatility = st.number_input('Volatility')
-
-
-st.text(blackscholes(spot, strike, risk_free, time/365, volatility))
+cols = st.columns(2)
+with cols[0]:
+    st.text(blackscholes(spot, strike, risk_free, time, volatility))
 
 
 
